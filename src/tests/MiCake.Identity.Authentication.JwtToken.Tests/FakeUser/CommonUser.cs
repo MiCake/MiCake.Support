6084ac1090fa9101d836e28026791035e1f1d4b5
xiangjie.liu <xiangjie.liu@razer.com>
2022-10-09 17:08:03 +0800

    init projects

---
+++ src/tests/MiCake.Identity.Authentication.JwtToken.Tests/FakeUser/CommonUser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiCake.Identity.Authentication.JwtToken.Tests.FakeUser
+{
+    public class CommonUser : IMiCakeUser<long>
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public CommonUser()
+        {
+
+        }
+    }
+
+    public class UserWithJwtClaim : IMiCakeUser<Guid>
+    {
+        [JwtClaim(ClaimName = "userid")]
+        public Guid Id { get; set; }
+
+        [JwtClaim()]
+        public string Name { get; set; }
+
+        public UserWithJwtClaim()
+        {
+        }
+    }
+
+    public class FakeCurrentUser_Guid : CurrentMiCakeUser<Guid>
+    {
+        public override Guid GetUserID()
+        {
+            return Guid.NewGuid();
+        }
+    }
+
+    public class FakeCurrentUser_long : CurrentMiCakeUser<long>
+    {
+        public override long GetUserID()
+        {
+            return 1001;
+        }
+    }
+}
